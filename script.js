1e41be4e2937ffb189d05ea6dda4313e60bc8908
lara <azizasayedahmad@gmail.com>
2021-11-11 00:45:19 +0200

    AddMember-withReset

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ function showContent() {
     let users = document.getElementById("users");
     users.appendChild(clon);
     document.getElementById("member_name").innerHTML = name;
-    //  document.getElementById("add").innerHTML = " ";
-    // name.reset();
+    document.getElementById("add").value = " ";
     // }
 }
 
@@ -24,5 +23,18 @@ function iseMpty(input) {
 //add.addEventListener("click", showContent);
 
 function removeDummy(e) {
-    e.parentNode.parentNode.removeChild(e.parentNode); 
+    e.parentNode.parentNode.removeChild(e.parentNode);
 }
+
+/*function myFunction() {
+    let input = document.getElementById("search");
+    let filter = input.value.toUpperCase();
+    for (let i = 0; i < document.getElementsByClassName('members').length; i++) {
+        if (document.getElementsByClassName('members'[i].innerHTML.toUpperCase().indexOf(filter) > -1)) {
+            document.getElementsByClassName("members")[i].style.display = "none";
+        }
+        else {
+            document.getElementsByClassName("members")[i].style.display = "";
+        }
+    }
+}*/
